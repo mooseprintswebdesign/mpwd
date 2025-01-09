543d8ae91fd77bbce82bcfe7f6ce4f66cfc46409
Abigail Dawson <abigaildawson.dev@gmail.com>
2025-01-09 15:30:04 -0500

    Update output directory to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.mooseprintswebdesign.com',
   integrations: [mdx(), sitemap()],
-  buildOptions: {
-    out: 'dist',
-  },
+  output: 'static',
 });
